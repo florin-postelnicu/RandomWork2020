ee59a91951d954c081a713f4965b29c18d2ea3e6
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2020-06-11 10:12:42 -0700

    Add files via upload

--- CollectOneD.java
+++ CollectOneD.java
@@ -5,10 +5,10 @@
 Original Problem :
 There are 3 jars. The first jar ( a)  can hold p litters, the second jar (b)
 can hold q litters, and the third jar (c) contains p + q litters.
-p, q are relatively prime numbers.
+p, q are natural numbers having the greatest common divisor d.
 At the beginning jars a, and b are empty(0 litters) and jar c has p + q litters.
-The objective is to obtain exactly 1 litter by transferring water from one jar to another.
-You are not allowed to discharge any water.
+The objective is to obtain exactly d litters by transferring watter from one jar to another.
+You are not allowed to discharge any watter.
 For example : a = 5 litters capacity
               b = 7 litters capacity
               c = 12 litters capacity.
@@ -19,6 +19,21 @@ For example : a = 5 litters capacity
               At step 3
               a = 5 , b = 2 , c = 5
               etc.
+
+Another example:
+            p is
+            12
+            q is
+            20
+             gcd of 12  and  20  is 4
+             Step 0  a = 0   b = 0   c = 32
+             Step 1  a = 0   b = 20   c = 12
+             Step 2  a = 12   b = 8   c = 12
+             Step 3  a = 0   b = 8   c = 24
+             Step 4  a = 8   b = 0   c = 24
+             Step 5  a = 8   b = 20   c = 4
+
+
  */
 
 import java.util.Scanner;
@@ -33,7 +48,7 @@ public class CollectOneD {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter values for p and q, p < q");
+        System.out.println("Enter values for p and q, natural numbers");
         System.out.println("p is  ");
         int p = scan.nextInt();
         System.out.println("q is ");
