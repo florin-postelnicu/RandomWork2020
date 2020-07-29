d0e9c8c145c7cb808ab8a5166b560f83d28e091c
florin-postelnicu <30669906+florin-postelnicu@users.noreply.github.com>
2020-07-29 08:54:21 -0700

    Update Pilgrim.java

--- Pilgrim.java
+++ Pilgrim.java
@@ -14,37 +14,27 @@ public class Pilgrim{
     public static ArrayList<Integer> Elimination_Method(ArrayList<Integer> listCheck, int skip){
 
         ArrayList<Integer> listB = new ArrayList<>();
-
         ArrayList<Integer> lista;
+        
         for( int k = 0; k < listCheck.size(); k ++){
-
             if (( k+1)%skip != 0) listB.add(listCheck.get(k));
-
         }
 
         lista = (ArrayList<Integer>) listB.clone();
-
         int endof = listB.size();
         int permute = listCheck.size()%skip;
         for (int index = 0; index < endof ; index++) lista.set((index + permute) % endof, listB.get(index));
         return lista;
-
     }
-
-
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner((System.in));
-
         System.out.println("Enter the number of knights  ");
-
         int knights = scan.nextInt();
         System.out.println("Enter the pace  ");
         int pace = scan.nextInt();
-
         System.out.println(" Knights  " + knights);
-
         ArrayList<Integer> listA = new ArrayList<>();
 
         for (int j = 1; j <= knights; j++) { listA.add(j);}
